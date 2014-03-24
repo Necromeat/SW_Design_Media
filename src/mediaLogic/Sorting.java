6cc020f1bf307b4ba94b189991c1390d9d8b4e96
Necromeat <lars_andrew@hotmail.com>
2014-03-24 12:52:29 +0100

    Test Round 1 ResultList pass

--- src/mediaLogic/Sorting.java
+++ src/mediaLogic/Sorting.java
@@ -6,30 +6,130 @@
 
 package mediaLogic;
 
+
 /**
  *
  * @author Andrew
  */
-public class Sorting implements interfaces.SortingInterface{
+public class Sorting<E> implements interfaces.SortingInterface<E>{
 
     @Override
-    public Object[] sortByAlphabet(Object[] e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public < E extends Comparable<E> > E[] sortByAlphabet(E[] e) {
+        E[] es = null; 
+       return es =(E[])sortSmallerThan(e);
+    
     }
 
     @Override
-    public Object[] sortByDate(Object[] e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public< E extends Comparable<E> > E[] sortByDate(E[] e) {
+     E[] es = null; 
+       return es =(E[])sortSmallerThan(e); 
     }
 
     @Override
-    public Object[] sortBySize(Object[] e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public< E extends Comparable<E> > E[] sortBySize(E[] e) {
+     E[] es = null; 
+       return es =(E[])sortSmallerThan(e);
     }
 
+    
+
     @Override
-    public Object[] sortByLength(Object[] e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public <E extends Comparable<E>> E[] sortByLength(E[] e) {
+       E[] es = null; 
+       return es =(E[])sortSmallerThan(e);
     }
     
+    <E extends Comparable<E>> E[] sortSmallerThan(E[] e){
+         E[] es; 
+        if(e.length >0){
+           es = e;
+        }else{
+            return e;
+        }
+      int min;
+        int current;
+        E temp;
+
+        for (int i = 0; i < es.length - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < es.length; j++) {
+                current = j;
+                if (es[min].compareTo(e[current]) < 0) {
+                    min = current;
+
+                }
+
+                temp = es[min];
+                es[min] = es[current];
+               es[current] = temp;
+                
+            }
+
+        }
+        
+        return es;
+    }
+    
+    <E extends Comparable<E>> E[] sortLargerThan(E[] e){
+         E[] es; 
+        if(e.length >0){
+           es = e;
+        }else{
+            return e;
+        }
+      int min;
+        int current;
+        E temp;
+
+        for (int i = 0; i < es.length - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < es.length; j++) {
+                current = j;
+                if (es[min].compareTo(e[current]) > 0) {
+                    min = current;
+
+                }
+
+                temp = es[min];
+                es[min] = es[current];
+               es[current] = temp;
+                
+            }
+
+        }
+        
+        return es;
+    }
+    
+    <E extends Comparable<E>> E[] sortEqualTo(E[] e){
+         E[] es; 
+        if(e.length >0){
+           es = e;
+        }else{
+            return e;
+        }
+      int min;
+        int current;
+        E temp;
+
+        for (int i = 0; i < es.length - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < es.length; j++) {
+                current = j;
+                if (es[min].compareTo(e[current]) == 0) {
+                    min = current;
+
+                }
+
+                temp = es[min];
+                es[min] = es[current];
+               es[current] = temp;
+                
+            }
+
+        }
+        
+        return es;
+    }
 }
